4b31a4ec4b09a84098767e1e3fe003226754b29d
Margarita Shishkova <margolen239@gmail.com>
2023-02-03 17:17:43 +0000

    To do marvelheroes

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import "./Card.css";
+
+
+
+export default function Card(props) {
+  return (
+    <div className="hero_card">
+
+        <div className="hero-name">{props.name}</div>
+        <img src={props.url}/> 
+        <div className="hero-universe">{props.universe}</div>
+        <div className="hero-alter_ego">{props.alter_ego}</div>
+        <div className="hero-type_of_activity">{props.activity}</div>
+        <div className="hero-friends">{props.friends}</div>
+        <div className="hero-superpowers">{props.superpowers}</div> 
+
+    </div>
+  )
+}
